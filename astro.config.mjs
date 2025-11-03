3ecf2680241dfd41717ce386abae765efa1af8b1
Daniel Lang <danslang@gmail.com>
2025-11-03 15:34:19 -0600

    Add GitHub Pages deploy config and site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,10 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://danlang422.github.io/fullstack-dev-bootcamp-notes/',
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'Notes on Astro',
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
 				{ label: 'HTML', autogenerate: { directory: 'html' } },
