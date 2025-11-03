3000b237049f65d47fb52e53474785af762c08ae
Daniel Lang <danslang@gmail.com>
2025-11-03 15:46:36 -0600

    Fix to config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,16 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
-
-// https://astro.build/config
 export default defineConfig({
-	site: 'https://danlang422.github.io/fullstack-dev-bootcamp-notes/',
+	site: 'https://danlang422.github.io/fullstack-dev-bootcamp-notes',
 	base: '/fullstack-dev-bootcamp-notes/',
 	integrations: [
-		starlight({
-			title: 'Notes on Astro',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{ label: 'HTML', autogenerate: { directory: 'html' } },
-				{ label: 'CSS', autogenerate: { directory: 'css' } },
-				{ label: 'JavaScript', autogenerate: { directory: 'javascript' } },
-			  ]
-			  
-		}),
+	  starlight({
+		title: 'Notes on Astro',
+		social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/danlang422/fullstack-dev-bootcamp-notes' }],
+		sidebar: [
+		  { label: 'HTML', autogenerate: { directory: 'html' } },
+		  { label: 'CSS', autogenerate: { directory: 'css' } },
+		  { label: 'JavaScript', autogenerate: { directory: 'javascript' } },
+		],
+	  }),
 	],
-});
+  });
+  
