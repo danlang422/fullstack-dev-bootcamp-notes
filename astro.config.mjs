9748da968d0cedefbb0efb7e760c6163f67bb112
Daniel Lang <danslang@gmail.com>
2025-11-03 15:48:16 -0600

    Fixed config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,22 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
 export default defineConfig({
-	site: 'https://danlang422.github.io/fullstack-dev-bootcamp-notes',
-	base: '/fullstack-dev-bootcamp-notes/',
-	integrations: [
-	  starlight({
-		title: 'Notes on Astro',
-		social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/danlang422/fullstack-dev-bootcamp-notes' }],
-		sidebar: [
-		  { label: 'HTML', autogenerate: { directory: 'html' } },
-		  { label: 'CSS', autogenerate: { directory: 'css' } },
-		  { label: 'JavaScript', autogenerate: { directory: 'javascript' } },
-		],
-	  }),
-	],
-  });
-  
+  site: 'https://danlang422.github.io/fullstack-dev-bootcamp-notes', // note: no trailing slash
+  base: '/fullstack-dev-bootcamp-notes/',
+  integrations: [
+    starlight({
+      title: 'Notes on Astro',
+      social: [
+        { icon: 'github', label: 'GitHub', href: 'https://github.com/danlang422/fullstack-dev-bootcamp-notes' },
+      ],
+      sidebar: [
+        { label: 'HTML', autogenerate: { directory: 'html' } },
+        { label: 'CSS', autogenerate: { directory: 'css' } },
+        { label: 'JavaScript', autogenerate: { directory: 'javascript' } },
+      ],
+    }),
+  ],
+});
